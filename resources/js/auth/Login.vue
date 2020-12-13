f6ebf4c3973228bb50fed275e1a0aca72747b163
iksor <iksorek@gmail.com>
2020-12-13 22:00:33 +0000

    ADD Login method
    Signed-off-by: iksor <iksorek@gmail.com>

--- resources/js/auth/Login.vue
+++ resources/js/auth/Login.vue
@@ -8,18 +8,24 @@
                     <input type="text" id="email" name="email"
                            placeholder="Enter email"
                            v-model="email"
-                           class="form-control">
-
+                           class="form-control"
+                           :class="[{'is-invalid': errorFor('email')}]">
+                    <v-errors :errors="errorFor('email')"></v-errors>
                 </div>
+
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password"
                            placeholder="Enter your password"
                            v-model="password"
-                           class="form-control">
+                           class="form-control"
+                           :class="[{'is-invalid': errorFor('password')}]">
+                    <v-errors :errors="errorFor('password')"></v-errors>
 
                 </div>
-                <button type="submit" class="btn-outline-primary btn btn-block">Login</button>
+                <button type="submit" :disabled="loding" @click.prevent="login"
+                        class="btn-outline-primary btn btn-block">Login
+                </button>
             </form>
             <hr>
             <span class="text-nowrap">
@@ -37,12 +43,36 @@
 </template>
 
 <script>
+import validationErrors from "../shared/mixins/validationErrors";
+
 export default {
     name: "Login",
+    mixins: [validationErrors],
     data() {
         return {
             email: null,
             password: null,
+            loding: false,
+        }
+    },
+    methods: {
+        async login() {
+            this.loding = true;
+            this.errors = null;
+            try {
+
+                await axios.get('/sanctum/csrf-cookie');
+                await axios.post("/login", {
+                    email: this.email,
+                    password: this.password
+                });
+            } catch (error) {
+                this.errors = error.response && error.response.data.errors;
+                this.loding = false;
+            }
+            await axios.get('/user');
+
+
         }
     }
 }
